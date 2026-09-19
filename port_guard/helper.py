733f9b431dd3153ccdbdaae340c4a047ef824249
atlegang-dotcom <atlegang.finiks@gmail.com>
2026-09-19 16:19:40 +0200

    feat: added ip_filter helper function, to filter between IP and IPv6 ip addresses

--- port_guard/helper.py
+++ port_guard/helper.py
@@ -1,4 +1,5 @@
 import ipaddress as ip
+from scapy.all import IP, IPv6
 
 # Checks/validates ip address, returns True if it's a valid ip address, false if not
 def check_ip(ip_addr) -> bool:
@@ -16,3 +17,14 @@ def filter_hits(hits: list, window: float, now: float) -> bool:
             result.append([hit[0], hit[1]])
 
     return result
+
+# filter between IPv6 and IPv4, by checking each for the ip layer they belong too, returns an IP address of the layer that is found
+def ip_filter(packet) -> str:
+    if packet.haslayer(IP):
+        return packet[IP].src
+        
+    elif packet.haslayer(IPv6):
+        return packet[IPv6].src
+
+    else:
+        return None
